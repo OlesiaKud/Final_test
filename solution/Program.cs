116951c401f9b0f84d39738d006d2c5dced6cc5f
Olesya Kudr <ala.kud00@mail.ru>
2023-07-13 15:39:18 +0700

    Ввод данных, опредение размера resultArr

---
+++ solution/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача: 
+// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
+// коллекциями, лучше обойтись исключительно массивами.
+
+// string [] ArrWithLessThen3Symbols(string [] arr1, string [] arr2)
+// {
+//     for (int i = 0; i < arr2.Length + 1; i++)
+//     {
+//         for (int j = 0;  j < arr1.Length + 1; j++)
+//         {
+//             if (arr1[j].Length <= 3)
+//             {
+//                 arr2[i] = arr1[j];
+//             }
+//         }     
+//     }
+//     return arr2;
+// }
+
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string [] arr= new string[size];
+int count = 0;
+
+for (int n = 0; n < size; n++)
+{
+    Console.Write($"Введите {(n + 1)} элемент массива: ");
+    arr[n] = Console.ReadLine();
+    if (arr[n].Length <= 3)
+    {
+        count++;
+    }
+}
+Console.Write($"{count}");
+string [] resultArr = new string[count];
+// Console.WriteLine("Массив из строк, длина которых меньше или равна 3:");
+// resultArr = ArrWithLessThen3Symbols(arr, resultArr);
+// Console.WriteLine(string.Join(", ", resultArr));
